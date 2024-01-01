df4bea639baf0ff624080aa657cf8c5e29e5e428
sorahei <wr_sorarara@yahoo.co.jp>
2024-01-02 00:56:48 +0800

    change to this responsibility

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-component-ios
+//
+//  Created by heihei on 2024/1/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift_component_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_component_iosApp.swift
+//  swift-component-ios
+//
+//  Created by heihei on 2024/1/2.
+//
+
+import SwiftUI
+
+@main
+struct swift_component_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
