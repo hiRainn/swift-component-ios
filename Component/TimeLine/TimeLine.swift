2a6d640d83d9caabc984283b7640dff6408e46f3
sorahei <wr_sorarara@yahoo.co.jp>
2024-01-27 23:09:00 +0800

    time line

--- Component/TimeLine/TimeLine.swift
+++ Component/TimeLine/TimeLine.swift
@@ -20,17 +20,15 @@ struct TimeLineView: View {
     }
 
     func onAppearEvent() {
-        self.dataList = [
-            timeLineData(createAt: Date.setDate("2014-01-06") ?? Date(), title: "title1", content: "conten1", isFinish: false, tagColor: .red),
-            timeLineData(createAt: Date.setTime("2014-01-06 10:12:11") ?? Date(), title: "title1", content: "conten1", isFinish: false, tagColor: .green),
-            timeLineData(createAt: Date.setTime("2014-01-08 10:12:11") ?? Date(), title: "title1", content: "conten1", isFinish: false, tagColor: .green),
-            timeLineData(createAt: Date.setTime("2014-01-12 10:12:11") ?? Date(), title: "title1", content: "conten1", isFinish: false, tagColor: .green),
-        ]
+        
     }
 
 }
 
 #Preview {
     TimeLineView()
+        .onAppear(){
+            initDataList()
+        }
 }
 
